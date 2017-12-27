87a474a275f3491c1ae1b64d389f5ecba953448e
Dhruv Amin <amin.dhruv@gmail.com>
2017-12-27 12:58:04 -0500

    Add comments, draw tree for better understanding

--- Python/lowest_common_ancestor.py
+++ Python/lowest_common_ancestor.py
@@ -19,6 +19,15 @@ if __name__ == '__main__':
     root = BSTNode(10)
     root.left = BSTNode(-10, None, BSTNode(8, BSTNode(6), BSTNode(9)))
     root.right = BSTNode(30, BSTNode(25, None, BSTNode(28)), BSTNode(60, None, BSTNode(78)))
-    print(lowest_common_ancestor(root, root.right.left.right, root.right.right.right))
-    print(lowest_common_ancestor(root, root.left.right.left, root.left.right.right))
-    print(lowest_common_ancestor(root, root.right, root.right.right.right))
+    """
+         10
+       /    \
+    -10     30
+      \     / \
+       8   25 60
+      / \   \   \
+     6   9   28  78
+    """
+    print(lowest_common_ancestor(root, root.right.left.right, root.right.right.right))  # 28 78 -> 30
+    print(lowest_common_ancestor(root, root.left.right.left, root.right))  # 6 30 -> 10
+    print(lowest_common_ancestor(root, root.right, root.right.right.right))  # 30 78 -> 30
