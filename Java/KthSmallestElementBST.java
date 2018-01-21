5ef4d5d67a60d9531c06b122064fcbbb049094dc
Dhruv Amin <amin.dhruv@gmail.com>
2018-01-21 00:16:37 -0500

    Rename for clarity

--- Java/KthSmallestElementBST.java
+++ Java/KthSmallestElementBST.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-public class KthSmallestElement {
+public class KthSmallestElementBST {
     static class TreeNode {
         int value;
         TreeNode left, right;
@@ -16,16 +16,15 @@ public class KthSmallestElement {
     }
 
     public static void main(String[] args) {
-        KthSmallestElement obj = new KthSmallestElement();
         TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(6, new TreeNode(5), new TreeNode(7)));
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter value of k: ");
         int k = sc.nextInt();
         sc.close();
-        System.out.println("Result: " + obj.kthSmallestElement(root, k));
+        System.out.println("Result: " + kthSmallestElement(root, k));
     }
 
-    private int kthSmallestElement(TreeNode root, int k) {
+    private static int kthSmallestElement(TreeNode root, int k) {
         int count = 0;
         TreeNode current = root;
         Stack<TreeNode> stack = new Stack<>();
