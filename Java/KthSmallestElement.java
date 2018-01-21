4cab016f95d9e1e88351b8839128e874aac7650d
Dhruv Amin <amin.dhruv@gmail.com>
2018-01-21 00:14:36 -0500

    Find kth smallest element in Binary Search Tree

---
+++ Java/KthSmallestElement.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class KthSmallestElement {
+    static class TreeNode {
+        int value;
+        TreeNode left, right;
+        TreeNode(int value) {
+            this.value = value;
+        }
+        TreeNode(int value, TreeNode left, TreeNode right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        KthSmallestElement obj = new KthSmallestElement();
+        TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(6, new TreeNode(5), new TreeNode(7)));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of k: ");
+        int k = sc.nextInt();
+        sc.close();
+        System.out.println("Result: " + obj.kthSmallestElement(root, k));
+    }
+
+    private int kthSmallestElement(TreeNode root, int k) {
+        int count = 0;
+        TreeNode current = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || current != null) {
+            if (current != null) {
+                stack.push(current);
+                current = current.left;
+            } else {
+                TreeNode node = stack.pop();
+                if (++count == k)
+                    return node.value;
+                current = node.right;
+            }
+        }
+        return Integer.MIN_VALUE;
+    }
+}
