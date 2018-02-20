d959c02fc5be83996365abfac47761c7becaee24
Dhruv Amin <amin.dhruv@gmail.com>
2018-02-20 00:21:56 -0500

    Change to Python3

--- Python/coin_change.py
+++ Python/coin_change.py
@@ -3,14 +3,14 @@ def ways_to_make_coin_change(denominations, amount):
     # where m = number of denominations and n = amount
     if len(denominations) == 0 or amount < 0:
         return 0
-    cache = [0 for i in xrange(amount + 1)]
+    cache = [0 for i in range(amount + 1)]
     cache[0] = 1  # Edge case when amount is zero
     for coin in denominations:
-        for next_amount in xrange(coin, amount + 1):
+        for next_amount in range(coin, amount + 1):
             remainder = next_amount - coin
             cache[next_amount] += cache[remainder]
     return cache[amount]
 
 
 if __name__ == '__main__':
-    print ways_to_make_coin_change([1, 6, 10], 12)
+    print(ways_to_make_coin_change([1, 6, 10], 12))
