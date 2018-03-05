e1132e92425725fa7a5e4e8fd0e58d14f5ac2ec2
Dhruv Amin <amin.dhruv@gmail.com>
2018-03-05 00:38:43 -0500

    Define TreeNode

--- Java/TreeTraversal.java
+++ Java/TreeTraversal.java
@@ -1,12 +1,12 @@
 public class TreeTraversal {
     public static void main(String[] args) {
-        CreateBST.Node root = new CreateBST.Node(5);
-        root.left = new CreateBST.Node(1);
-        root.right = new CreateBST.Node(9);
-        root.left.left = new CreateBST.Node(0);
-        root.left.right = new CreateBST.Node(4);
-        root.right.left = new CreateBST.Node(7);
-        root.right.right = new CreateBST.Node(3);
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(9);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(3);
         System.out.println("In order traversal:");
         inOrderTraversal(root);
         System.out.println();
@@ -17,24 +17,30 @@ public class TreeTraversal {
         postOrderTraversal(root);
     }
 
-    private static void inOrderTraversal(CreateBST.Node root) {
+    private static void inOrderTraversal(TreeNode root) {
         if (root == null) return;
         inOrderTraversal(root.left);
         System.out.print(root.data + " ");
         inOrderTraversal(root.right);
     }
 
-    private static void preOrderTraversal(CreateBST.Node root) {
+    private static void preOrderTraversal(TreeNode root) {
         if (root == null) return;
         System.out.print(root.data + " ");
         preOrderTraversal(root.left);
         preOrderTraversal(root.right);
     }
 
-    private static void postOrderTraversal(CreateBST.Node root) {
+    private static void postOrderTraversal(TreeNode root) {
         if (root == null) return;
         postOrderTraversal(root.left);
         postOrderTraversal(root.right);
         System.out.print(root.data + " ");
     }
+    
+    /*class TreeNode {
+        int data;
+        TreeNode left, right;
+        TreeNode(int data) { this.data = data; }
+    }*/
 }
